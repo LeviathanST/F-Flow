646e66dea392e94d1bc05fb16d3a311fdbe1d647
LeviathanST <phamquochung170505@gmail.com>
2025-04-02 02:34:42 +0700

    chore(config/database): config instead of connect

---
+++ src/main/kotlin/configs/DatabaseConfig.kt
@@ -0,0 +1,12 @@
+package configs
+
+import annotations.EnvVar
+
+data class DatabaseConfig(
+    @EnvVar(name = "DB_URL")
+    var url: String = "",
+    @EnvVar(name = "DB_USERNAME")
+    var username: String = "",
+    @EnvVar(name = "DB_PASSWORD")
+    var password: String = "",
+)
