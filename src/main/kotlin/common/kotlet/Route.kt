8111c618324daafc1b3c3190439a7f33d6edadab
LeviathanST <phamquochung170505@gmail.com>
2025-04-02 02:29:37 +0700

    chore(kotlet): change dir

--- src/main/kotlin/common/kotlet/Route.kt
+++ src/main/kotlin/common/kotlet/Route.kt
@@ -1,34 +1,53 @@
-package common
+package common.kotlet
 
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 
 typealias RouteHandler = (HttpServletRequest, HttpServletResponse) -> Unit
 
-data class Routing(val method: String, val path: String, val handler: RouteHandler)
+data class Routing(
+    val method: String,
+    val path: String,
+    val handler: RouteHandler,
+)
 
 class Router {
     private val routes: MutableList<Routing> = mutableListOf()
 
-    fun get(path: String, handler: RouteHandler) {
+    fun get(
+        path: String,
+        handler: RouteHandler,
+    ) {
         routes.add(Routing("GET", path, handler))
     }
-    fun post(path: String, handler: RouteHandler) {
+
+    fun post(
+        path: String,
+        handler: RouteHandler,
+    ) {
         routes.add(Routing("POST", path, handler))
     }
-    fun handle(req: HttpServletRequest, res: HttpServletResponse) {
+
+    // Get all routes exist in application then compare with current request's path and method
+    fun handle(
+        req: HttpServletRequest,
+        res: HttpServletResponse,
+    ) {
         val path = req.pathInfo?.substring(1) ?: ""
         val method = req.method
         val route = routes.find { it.method == method && it.path == path }
         route?.handler?.invoke(req, res)
-                ?: res.sendError(
-                        HttpServletResponse.SC_NOT_FOUND,
-                        "Route not found: $method - $path"
-                )
+            ?: res.sendError(
+                HttpServletResponse.SC_NOT_FOUND,
+                "Route not found: $method - $path",
+            )
     }
+
+    // Register a group of routes
     fun addProvider(provider: RouteProvider) {
         provider.build(this)
     }
+
     fun register(block: Router.() -> Unit) {
         this.apply(block)
     }
--- src/main/kotlin/common/kotlet/RouteProvider.kt
+++ src/main/kotlin/common/kotlet/RouteProvider.kt
@@ -1,4 +1,4 @@
-package common
+package common.kotlet
 
 interface RouteProvider {
     // Route register
