fed3d9b01c9b7fb06d12dc34406eba85860dda82
LeviathanST <phamquochung170505@gmail.com>
2025-04-02 23:24:42 +0700

    update(home): change import path kotlet

--- src/main/kotlin/Home.kt
+++ src/main/kotlin/Home.kt
@@ -1,10 +1,13 @@
 import auth.AuthController
-import common.Kotlet
-import common.Router
+import common.kotlet.Kotlet
+import common.kotlet.Router
+import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.servlet.annotation.WebServlet
 
 @WebServlet(urlPatterns = ["/app/*"])
 class Home : Kotlet() {
+    private val logger = KotlinLogging.logger {}
+
     override fun build(router: Router) {
         router.addProvider(AuthController())
         router.register {
