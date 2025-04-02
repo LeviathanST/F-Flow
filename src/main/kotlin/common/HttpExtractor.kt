678e772f1f18239501314e7342159ca3a0a9e0b4
LeviathanST <phamquochung170505@gmail.com>
2025-04-02 23:23:46 +0700

    update(http_extractor): return result

--- src/main/kotlin/common/HttpExtractor.kt
+++ src/main/kotlin/common/HttpExtractor.kt
@@ -2,11 +2,15 @@ package common
 
 import com.google.gson.Gson
 import jakarta.servlet.http.HttpServletRequest
+import kotlin.runCatching
 
 class HttpExtractor {
-    inline fun <reified T> extractBody(req: HttpServletRequest): T {
+    // / Extract body content from the request
+    inline fun <reified T : Any> extractBody(req: HttpServletRequest): Result<T, Throwable> {
         val gson = Gson()
-        val body = req.reader.use { it.readText() }
-        return gson.fromJson(body.toString(), T::class.java)
+        return runCatching {
+            val body = req.reader.use { it.readText() }
+            Ok(gson.fromJson(body.toString(), T::class.java))
+        }.getOrElse { err -> Err(err) }
     }
 }
