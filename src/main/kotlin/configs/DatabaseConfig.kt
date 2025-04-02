bc8bedc9936e6b15bfb73477be1745765544480c
LeviathanST <phamquochung170505@gmail.com>
2025-04-02 23:20:57 +0700

    update(database): change how to connect

--- src/main/kotlin/configs/DatabaseConfig.kt
+++ src/main/kotlin/configs/DatabaseConfig.kt
@@ -5,6 +5,8 @@ import annotations.EnvVar
 data class DatabaseConfig(
     @EnvVar(name = "DB_URL")
     var url: String = "",
+    @EnvVar(name = "DB_DATABASE")
+    var database: String = "",
     @EnvVar(name = "DB_USERNAME")
     var username: String = "",
     @EnvVar(name = "DB_PASSWORD")
