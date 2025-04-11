dba251aa5f54e44b89bf195866c6321c2af68958
LeviathanST <phamquochung170505@gmail.com>
2025-04-11 20:58:56 +0700

    update(auth): role repo as apart of the module

--- src/main/kotlin/auth/repositories/RoleRepository.kt
+++ src/main/kotlin/auth/repositories/RoleRepository.kt
@@ -1,5 +1,4 @@
-package role
-
+package auth.repositories
 import DataNotFound
 import common.Database
 import common.Err
@@ -8,25 +7,23 @@ import common.Result
 import io.tekniq.jdbc.selectOne
 
 class RoleRepository {
-    private val dataSource = Database().connect()
+    private val ds = Database().connect()
 
     fun findIdByUsername(username: String): Result<Int, Throwable> =
         runCatching {
             val query = """
                 SELECT r.id FROM role r
-                JOIN user_role ur ON ur.role_id = r.id
-                JOIN user_account ua ON ua.id = ur.account_id
-                WHERE ua.username = ?
+                    JOIN user_role ur ON ur.role_id = r.id
+                    JOIN user_account ua ON ua.id = ur.account_id
+                WHERE username = ?
             """
             val id =
-                dataSource.selectOne(query, username) {
+                ds.selectOne(query, username) {
                     it.getInt("id")
                 }
-
             if (id == null) {
-                throw DataNotFound("Role in the application", username)
+                throw DataNotFound("Application role", username)
             }
-
             Ok(id)
         }.getOrElse { Err(it) }
 }
